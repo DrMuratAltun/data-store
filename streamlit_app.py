75bb5f1478357262f09ab2afc566eaed33a12790
suskun354 <61706944+suskun354@users.noreply.github.com>
2024-05-28 09:39:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,6 @@ with open('style.css')as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
 
 
-#load dataset
-df=pd.read_csv('data.csv')
-
 st.sidebar.image("logo2.png")
 #filter date to view data
 with st.sidebar:
